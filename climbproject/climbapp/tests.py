a25a63d4cd03f888566f819b320d9e905774e988
mmichelon <maxmichelon15@gmail.com>
2018-10-22 19:19:20 -0700

    Added form and linked database

--- climbproject/climbapp/tests.py
+++ climbproject/climbapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ClimbModel
+ # Create your tests here.
+class ClimbTestCase(TestCase):
+    def setUp(self):
+        ClimbModel.objects.create(climb="lion")
+        ClimbModel.objects.create(climb="cat")
+     def test_Climb_to_string(self):
+        lion = ClimbModel.objects.get(climb="lion")
+        cat = ClimbModel.objects.get(climb="cat")
+        self.assertEqual(str(lion), 'lion')
+        self.assertEqual(str(cat), 'cat')
