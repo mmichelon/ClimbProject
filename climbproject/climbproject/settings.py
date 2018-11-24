508cf822a1eb0b78f7120a3d469ceb65e3b423f7
mmichelon <maxmichelon15@gmail.com>
2018-11-23 20:16:35 -0800

    Had piazza problem, server opens, no text display. Chat socket closed on webpage

--- climbproject/climbproject/settings.py
+++ climbproject/climbproject/settings.py
@@ -138,11 +138,22 @@ STATIC_URL = '/static/'
 #     },
 # }
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("redis://redis:6379/1", 6379)],
+#         },
+#     },
+# }
+
+# Channels
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis://redis:6379/1", 6379)],
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            # "hosts": [('127.0.0.1', 6379)],
+            "hosts": [("redis://redis:6379")]
         },
     },
 }
